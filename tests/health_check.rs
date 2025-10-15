0035aa02e077b46ca3d1fada11a5a51b4c33cf9d
Luca Zhang <feiya.zh@gmail.com>
2025-10-15 22:20:13 +0800

    Return 400 instead of 200 for empty fields

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -174,7 +174,7 @@ async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
             .expect("Failed to execute request.");
         // Assert
         assert_eq!(
-            200,
+            400,
             response.status().as_u16(),
             "The API did not return a 200 OK when the payload was {}.",
             description
